a7b059719635f570dff0bf15b3ade041f7e7ce2e
leandrofe-fernandez66 <leandrofernandez6716@gmail.com>
2025-02-16 21:48:20 -0300

    Creation of .env file

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,16 +3,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 //import { getAnalytics } from "firebase/analytics";
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const APP_ID = import.meta.env.VITE_APP_ID;
+const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID;
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqCAy8HG5bGP87cJTH1nWqxl8q5twy71s",
-  authDomain: "rick-and-morty-auth-98893.firebaseapp.com",
-  projectId: "rick-and-morty-auth-98893",
-  storageBucket: "rick-and-morty-auth-98893.firebasestorage.app",
-  messagingSenderId: "53296020287",
-  appId: "1:53296020287:web:0ff4513b174ed05891318a",
-  measurementId: "G-PBVV0RE7R4"
+  apiKey: `${API_KEY}`,
+  authDomain: `${AUTH_DOMAIN}`,
+  projectId: `${PROJECT_ID}`,
+  storageBucket: `${STORAGE_BUCKET}`,
+  messagingSenderId: `${MESSAGING_SENDER_ID}`,
+  appId: `${APP_ID}`,
+  measurementId: `${MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
